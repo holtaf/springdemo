60e123bd9e7c5fa681c792e43e83ae3f32685499
nazaryan <arthur.nazaryan.st@gmail.com>
2020-10-10 16:10:57 +0400

    Reactor implementation of task cancelling

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -5,48 +5,69 @@ import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @RestController
 public class GreetingController {
 	private final ManagableThreadPoolExecutor threadPoolExecutor;
 
+	private Map<String, Disposable> disposableMap;
+
 	public GreetingController() {
 		threadPoolExecutor = new ManagableThreadPoolExecutor();
+
+		disposableMap = new HashMap<>();
 	}
 
 	@GetMapping(value = "foo")
 	public Flux<ServerSentEvent<Object>> foo() {
-		return wrapResponse(UUID.randomUUID().toString(), this::startGeneratingValues);
+		return wrapResponse2(UUID.randomUUID().toString(), this::getData1);
+	}
+
+	private Flux<Event> getData1() {
+		return Flux.range(0, 10).delayElements(Duration.ofSeconds(1)).<Event>map(i -> new DataEvent(new Graph(i, "HELLO")))
+				.doOnNext(event -> System.out.println("Next event : " + event));
 	}
 
 	@GetMapping(value = "tasks/cancel/{id}")
 	public void cancelTask(@PathVariable("id") String id) {
-		threadPoolExecutor.cancel(id);
+//		threadPoolExecutor.cancel(id);
+
+		Disposable disposable = disposableMap.get(id);
+		if (disposable != null) {
+			disposable.dispose();
+			disposableMap.remove(id);
+		}
 	}
 
-	@GetMapping(value = "tasks/pause/{id}")
-	public void pauseTask(@PathVariable("id") String id) {
-		threadPoolExecutor.pause(id);
-	}
-
-	@GetMapping(value = "tasks/resume/{id}")
-	public void resumeTask(@PathVariable("id") String id) {
-		threadPoolExecutor.resume(id);
-	}
+//	@GetMapping(value = "tasks/pause/{id}")
+//	public void pauseTask(@PathVariable("id") String id) {
+//		threadPoolExecutor.pause(id);
+//	}
+//
+//	@GetMapping(value = "tasks/resume/{id}")
+//	public void resumeTask(@PathVariable("id") String id) {
+//		threadPoolExecutor.resume(id);
+//	}
 
 	@GetMapping(value = "tasks/get")
 	public Set<String> getTasks() {
-		return threadPoolExecutor.getTaskSet();
+		return disposableMap.keySet();
 	}
 
 	private Flux<ServerSentEvent<Object>> wrapResponse(String taskId, Consumer<FluxSink<Event>> sinkConsumer) {
@@ -64,9 +85,48 @@ public class GreetingController {
 			});
 		});
 
-//		Flux<Event> events =  Flux.create(sinkConsumer);
-
 		return Flux.concat(start, events).map(o -> ServerSentEvent.builder().data(o.getData()).event(o.getType()).build());
+	}
+
+	private Flux<ServerSentEvent<Object>> wrapResponse2(String taskId, Supplier<Flux<Event>> method) {
+		Mono<Event> start = Mono.just(new StartEvent(taskId));
+		Flux<Event> forwardedDataFlux = Flux.create(eventFluxSink -> {
+			Flux<Event> dataFlux = method.get();
+			Disposable disposable = dataFlux
+					.doOnCancel(new Runnable() {
+						@Override
+						public void run() {
+							eventFluxSink.next(new CancelEvent());
+							eventFluxSink.complete();
+
+							disposableMap.remove(taskId);
+						}
+					})
+					.doOnComplete(new Runnable() {
+						@Override
+						public void run() {
+							eventFluxSink.next(new CompleteEvent());
+							eventFluxSink.complete();
+
+							disposableMap.remove(taskId);
+						}
+					})
+					.doOnError(new Consumer<Throwable>() {
+						@Override
+						public void accept(Throwable throwable) {
+							eventFluxSink.next(new ExceptionEvent(throwable));
+							eventFluxSink.complete();
+
+							disposableMap.remove(taskId);
+						}
+					})
+					.doOnNext(eventFluxSink::next)
+					.subscribe();
+
+			disposableMap.put(taskId, disposable);
+		});
+
+		return Flux.concat(start, forwardedDataFlux).map(o -> ServerSentEvent.builder().data(o.getData()).event(o.getType()).build());
 	}
 
 	private void startGeneratingValues(FluxSink<Event> sink) {
@@ -162,7 +222,9 @@ public class GreetingController {
 		String getType();
 
 		@JsonIgnore
-		Object getData();
+		default Object getData() {
+			return null;
+		}
 	}
 
 	public static class StartEvent implements Event {
@@ -182,6 +244,27 @@ public class GreetingController {
 			return id;
 		}
 	}
+
+	public static class CancelEvent implements Event {
+		@Override
+		public String getType() {
+			return "cancel";
+		}
+
+		@Override
+		public Object getData() {
+			return null;
+		}
+	}
+
+	public static class CompleteEvent implements Event {
+		@Override
+		public String getType() {
+			return "complete";
+		}
+	}
+
+
 
 	public static class ProgressEvent implements Event {
 		public int progress;
